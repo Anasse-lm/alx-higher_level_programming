31ddc0eeb13de7a2611d1bc76ca6d2af482dc70a
Anasse-lm <fuctura.20@gmail.com>
2024-06-07 10:46:08 -0700

    task5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print(", ".join("0{}".format(i) if i < 10 else "{}".format(i) for i in range(0, 100)))
+print(", "
+        .join("0{}".format(i) if i < 10 else "{}".format(i) for i in range(0, 100)))
