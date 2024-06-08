903aa0d2909253af8676a7489e9cd6f951abe1ae
Anasse-lm <fuctura.20@gmail.com>
2024-06-08 14:10:00 -0700

    task11

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def pow(a, b):
     r = 1
-    for i in range(b):
+    p = b
+    if b < 0:
+        p = -b
+    for i in range(p):
         r = r * a
+    if b < 0:
+        r = 1 / r
     return r 
