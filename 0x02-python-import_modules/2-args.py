c93561c3de3ceee57e6d1249d7b86115353e83ec
Anasse-lm <fuctura.20@gmail.com>
2024-06-09 11:58:36 -0700

    task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 import sys
-if (len(sys.argv) - 1) == 0:
+argv = sys.argv
+if (len(argv) - 1) == 0:
     print("0 arguments.")
-elif (len(sys.argv) - 1) == 1:
+elif (len(argv) - 1) == 1:
     print("1 argument:")
-    print("1: {}".format(sys.argv[1]))
+    print("1: {}".format(argv[1]))
 else:
-    print("{} arguments:".format(len(sys.argv) - 1))
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(str(i), sys.argv[i]))
+    print("{} arguments:".format(len(argv) - 1))
+    for i in range(1, len(argv)):
+        print("{}: {}".format(str(i), argv[i]))
