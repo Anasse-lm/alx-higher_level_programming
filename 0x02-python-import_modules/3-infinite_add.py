2df6445c349628440782d1b05910dd1fb0cab0f3
Anasse-lm <fuctura.20@gmail.com>
2024-06-09 12:26:37 -0700

    task3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+def main():
+    argv = sys.argv
+    argc = len(argv)
+    if argc == 0:
+        print(0)
+    else:
+        result = 0
+        for i in range(1, argc):
+            result = result + int(argv[i])
+        print(result)
+if __name__ == "__main__":
+    main()
