416a433a23950b309cf978df6165b5834d7c809d
Anasse-lm <fuctura.20@gmail.com>
2024-06-09 12:03:52 -0700

    task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 import sys
-argv = sys.argv
-if (len(argv) - 1) == 0:
-    print("0 arguments.")
-elif (len(argv) - 1) == 1:
-    print("1 argument:")
-    print("1: {}".format(argv[1]))
-else:
-    print("{} arguments:".format(len(argv) - 1))
-    for i in range(1, len(argv)):
-        print("{}: {}".format(str(i), argv[i]))
+def main():
+    argv = sys.argv
+    if (len(argv) - 1) == 0:
+        print("0 arguments.")
+    elif (len(argv) - 1) == 1:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(str(i), argv[i]))
+if __name__ == "__main__":
+    main()
