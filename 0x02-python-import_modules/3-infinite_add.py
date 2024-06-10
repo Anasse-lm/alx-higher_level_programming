9a239afa16810fea8e7e13678f42376cda55d7f5
Anasse-lm <fuctura.20@gmail.com>
2024-06-10 11:26:02 -0700

    fix some issues with pycodestyle

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import sys
+
+
 def main():
     argv = sys.argv
     argc = len(argv)
@@ -10,5 +12,7 @@ def main():
         for i in range(1, argc):
             result = result + int(argv[i])
         print(result)
+
+
 if __name__ == "__main__":
     main()
